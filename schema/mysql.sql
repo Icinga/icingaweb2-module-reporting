1b4bc568886db9bf35ad90bd49fa96c30bb3689a
nmartini <niko.martini@netways.de>
2020-02-11 13:25:04 +0100

    initial commit - fixed that before

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -1,12 +1,4 @@
-CREATE TABLE template (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  author varchar(255) NOT NULL COLLATE utf8mb4_unicode_ci,
-  name varchar(128) NOT NULL COLLATE utf8mb4_unicode_ci,
-  settings longblob NOT NULL,
-  ctime bigint(20) unsigned NOT NULL,
-  mtime bigint(20) unsigned NOT NULL,
-  PRIMARY KEY(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
 
 CREATE TABLE timeframe (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
