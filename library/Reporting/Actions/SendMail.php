2856ec5d4c5b8d0ba6a310606bc938122fb6d92c
Johannes Meyer <johannes.meyer@icinga.com>
2022-01-11 14:51:55 +0100

    Make subject configurable per schedule
    resolves #78

--- library/Reporting/Actions/SendMail.php
+++ library/Reporting/Actions/SendMail.php
@@ -34,6 +34,10 @@ class SendMail extends ActionHook
 
         $mail->setFrom(Config::module('reporting')->get('mail', 'from', 'reporting@icinga'));
 
+        if (isset($config['subject'])) {
+            $mail->setSubject($config['subject']);
+        }
+
         switch ($config['type']) {
             case 'pdf':
                 $pdfexport = null;
@@ -108,13 +112,18 @@ class SendMail extends ActionHook
 
         $form->addElement('select', 'type', [
             'required'  => true,
-            'label'     => 'Type',
+            'label'     => t('Type'),
             'options'   => $types
+        ]);
+
+        $form->addElement('text', 'subject', [
+            'label'         => t('Subject'),
+            'placeholder'   => Mail::DEFAULT_SUBJECT
         ]);
 
         $form->addElement('textarea', 'recipients', [
             'required' => true,
-            'label'    => 'Recipients'
+            'label'    => t('Recipients')
         ]);
     }
 }
