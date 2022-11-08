57272f40d87e65ba808557ee46dad1e533f5ae0e
Timm Ortloff <timm.ortloff@icinga.com>
2022-11-08 08:36:23 +0100

    Add Select list and manual Email input to SendForm

--- library/Reporting/Web/Forms/SendForm.php
+++ library/Reporting/Web/Forms/SendForm.php
@@ -6,6 +6,7 @@ namespace Icinga\Module\Reporting\Web\Forms;
 
 use Icinga\Module\Reporting\Actions\SendMail;
 use Icinga\Module\Reporting\Database;
+use Icinga\Module\Reporting\Hook\EmailProviderHook;
 use Icinga\Module\Reporting\ProvidedReports;
 use Icinga\Module\Reporting\Report;
 use Icinga\Module\Reporting\Web\Forms\Decorator\CompatDecorator;
@@ -31,6 +32,34 @@ class SendForm extends CompatForm
         $this->setDefaultElementDecorator(new CompatDecorator());
 
         (new SendMail())->initConfigForm($this, $this->report);
+
+        $radio = $this->addElement('radio', 'source_radio', [
+            'label'   => 'E-Mail Source',
+            'options' => [
+                'manual'   => 'Manual',
+                'contacts' => 'Contacts',
+            ],
+            'value'   => 'contacts',
+            'class'   => 'autosubmit'
+        ]);
+
+        if ($radio->getValue('source_radio') === 'contacts') {
+            $emails = [null => 'Select Contacts'];
+            foreach (EmailProviderHook::getProvider() as $provider) {
+                var_dump($provider);
+                $emails = array_merge($emails, $provider->getContactEmails());
+            }
+
+            $this->addElement('select', 'emails_list', [
+                'multiple' => true,
+                'label'    => 'Contacts',
+                'options'  => $emails
+            ]);
+        } else {
+            $this->addElement('textarea', 'emails_manual', [
+                'label' => 'Contact E-Mails'
+            ]);
+        }
 
         $this->addElement('submit', 'submit', [
             'label' => $this->translate('Send Report')
