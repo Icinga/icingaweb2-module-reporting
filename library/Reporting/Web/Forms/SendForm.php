859526c4e76553dbba65a960528ca8d2a2222d0d
Timm Ortloff <timm.ortloff@icinga.com>
2023-01-31 10:35:19 +0100

    SendForm: translate texts

--- library/Reporting/Web/Forms/SendForm.php
+++ library/Reporting/Web/Forms/SendForm.php
@@ -10,12 +10,14 @@ use Icinga\Module\Reporting\Hook\EmailProviderHook;
 use Icinga\Module\Reporting\ProvidedReports;
 use Icinga\Module\Reporting\Report;
 use Icinga\Module\Reporting\Web\Forms\Decorator\CompatDecorator;
+use ipl\I18n\Translation;
 use ipl\Web\Compat\CompatForm;
 
 class SendForm extends CompatForm
 {
     use Database;
     use ProvidedReports;
+    use Translation;
 
     /** @var Report */
     protected $report;
@@ -34,29 +36,29 @@ class SendForm extends CompatForm
         (new SendMail())->initConfigForm($this, $this->report);
 
         $radio = $this->addElement('radio', 'source_radio', [
-            'label'   => 'E-Mail Source',
+            'label'   => $this->translate('E-Mail Source'),
             'options' => [
-                'manual'   => 'Manual',
-                'contacts' => 'Contacts',
+                'manual'   => $this->translate('Manual input'),
+                'contacts' => $this->translate('Contacts'),
             ],
             'value'   => 'contacts',
             'class'   => 'autosubmit'
         ]);
 
         if ($radio->getValue('source_radio') === 'contacts') {
-            $emails = [null => 'Select Contacts'];
+            $emails = [null => $this->translate('Select Contacts')];
             foreach (EmailProviderHook::getProviders() as $provider) {
                 $emails = array_merge($emails, $provider->getContactEmails());
             }
 
             $this->addElement('select', 'emails_list', [
                 'multiple' => true,
-                'label'    => 'Contacts',
+                'label'    => $this->translate('Contacts'),
                 'options'  => $emails
             ]);
         } else {
             $this->addElement('textarea', 'emails_manual', [
-                'label' => 'Contact E-Mails'
+                'label' => $this->translate('Contact E-Mails')
             ]);
         }
 
