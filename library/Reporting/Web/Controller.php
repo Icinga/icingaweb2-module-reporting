e6c921aef82d0b270cc58031ed3b3b52e02ffdef
Timm Ortloff <timm.ortloff@icinga.com>
2023-02-01 12:33:32 +0100

    Controller: Remove obsolete redirect

--- library/Reporting/Web/Controller.php
+++ library/Reporting/Web/Controller.php
@@ -4,19 +4,8 @@
 
 namespace Icinga\Module\Reporting\Web;
 
-use ipl\Html\Form;
 use ipl\Web\Compat\CompatController;
 
 class Controller extends CompatController
 {
-    protected function redirectForm(Form $form, $url)
-    {
-        if (
-            $form->hasBeenSubmitted()
-            && ((isset($form->valid) && $form->valid === true)
-                || $form->isValid())
-        ) {
-            $this->redirectNow($url);
-        }
-    }
 }
