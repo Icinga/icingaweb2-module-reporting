5b2161890db227b25e5fbdd2b751717794f951e1
Timm Ortloff <timm.ortloff@icinga.com>
2023-02-02 14:21:07 +0100

    EmailAddressBookHook: Add `getName()` and use Translation trait

--- library/Reporting/Hook/EmailAddressBookHook.php
+++ library/Reporting/Hook/EmailAddressBookHook.php
@@ -3,9 +3,12 @@
 namespace Icinga\Module\Reporting\Hook;
 
 use Icinga\Application\Hook;
+use ipl\I18n\Translation;
 
 abstract class EmailAddressBookHook
 {
+    use Translation;
+
     /**
      * Get a list of email addresses as email-label pairs
      *
@@ -19,5 +22,13 @@ abstract class EmailAddressBookHook
     final public static function getEmailAddressBooks(): array
     {
         return Hook::all('Reporting/EmailAddressBook');
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->translate('E-Mail Address Books');
     }
 }
