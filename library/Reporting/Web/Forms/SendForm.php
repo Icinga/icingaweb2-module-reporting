baf789b1ae70e315a3581168c9d80e8827d3db70
Timm Ortloff <timm.ortloff@icinga.com>
2023-02-02 13:40:47 +0100

    SendForm: Check if more than one contact is available

--- library/Reporting/Web/Forms/SendForm.php
+++ library/Reporting/Web/Forms/SendForm.php
@@ -6,7 +6,7 @@ namespace Icinga\Module\Reporting\Web\Forms;
 
 use Icinga\Module\Reporting\Actions\SendMail;
 use Icinga\Module\Reporting\Database;
-use Icinga\Module\Reporting\Hook\EmailProviderHook;
+use Icinga\Module\Reporting\Hook\EmailAddressBookHook;
 use Icinga\Module\Reporting\ProvidedReports;
 use Icinga\Module\Reporting\Report;
 use Icinga\Module\Reporting\Web\Forms\Decorator\CompatDecorator;
@@ -35,20 +35,24 @@ class SendForm extends CompatForm
 
         (new SendMail())->initConfigForm($this, $this->report);
 
+        $emailAddressBooks = EmailAddressBookHook::getEmailAddressBooks();
+
         $this->addElement('radio', 'source_radio', [
-            'label'   => $this->translate('E-Mail Source'),
-            'options' => [
+            'label'       => $this->translate('E-Mail Source'),
+            'options'     => [
                 'manual'   => $this->translate('Manual input'),
                 'contacts' => $this->translate('Contacts'),
             ],
-            'value'   => 'contacts',
-            'class'   => 'autosubmit'
+            'disabled'    => count($emailAddressBooks) === 0,
+            'value'       => 'contacts',
+            'class'       => 'autosubmit',
+            'description' => count($emailAddressBooks) === 0 ? $this->translate("No contacts available") : null
         ]);
 
-        if ($this->getPopulatedValue('source_radio', 'contacts') === 'contacts') {
+        if ($this->getPopulatedValue('source_radio', 'manual') === 'contacts') {
             $emails = [null => $this->translate('Select Contacts')];
-            foreach (EmailProviderHook::getProviders() as $provider) {
-                $emails = array_merge($emails, $provider->getContactEmails());
+            foreach ($emailAddressBooks as $addressBook) {
+                $emails = array_merge($emails, $addressBook->getContactEmails());
             }
 
             $this->addElement('select', 'emails_list', [
